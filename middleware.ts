038bb4c143dcd4738a7ab09a1704894542f8b654
Francisco Barragan <fbarragan@my.gcu.edu>
2024-07-02 23:10:48 -0700

    Added Supabase Auth and General Sign-up/Sign-in page

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import { updateSession } from '@/utils/supabase/middleware'
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  // List of always allowed paths
+  const allowedPaths = ['/', '/register', '/login', '/about', '/ui/homepage', '/api/admins/stats'];
+
+  // Allow navigation to any of the allowed paths
+  if (allowedPaths.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  return await updateSession(request)
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
---
+++ utils/auth-helpers/server.ts
@@ -0,0 +1,28 @@
+'use server';
+
+import { createClient } from '@/utils/supabase/server';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+// import { getURL, getErrorRedirect, getStatusRedirect } from 'utils/helpers';
+// import { getAuthTypes } from 'utils/auth-helpers/settings';
+
+function isValidEmail(email: string) {
+  var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return regex.test(email);
+}
+
+// Function to log user out
+export async function SignOut(formData: FormData) {
+  const pathName = String(formData.get('pathName')).trim();
+
+  const supabase = createClient();
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
