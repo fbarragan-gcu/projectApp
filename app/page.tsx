d7e80917685145d65fce209b61533d3ff57ce523
Francisco Barragan <fbarragan@my.gcu.edu>
2024-07-03 09:30:04 -0700

    Added supabase queries.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
+import { createClient } from "@/utils/supabase/server";
 import HomePage from "./ui/homepage/page";
 
 export default function Home() {
-  return (
-      <HomePage />
-  );
+  return <HomePage />;
 }
---
+++ utils/supabase/queries.ts
@@ -0,0 +1,18 @@
+import { createClient } from "@/utils/supabase/server";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { cache } from "react";
+
+export const getUser = cache( async (supabase: SupabaseClient) => {
+    const {
+        data: { user }
+    } = await supabase.auth.getUser();
+    return user;
+});
+
+export const getUserDetails = cache(async (supabase: SupabaseClient) => {
+    const { data: userDetails } = await supabase
+        .from('users')
+        .select('*')
+        .single();
+    return userDetails;
+});
