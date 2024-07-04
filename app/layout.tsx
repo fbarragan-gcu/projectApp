995ecb4aecf4414b83b8e9dc6c056cba2e45aa43
Francisco Barragan <fbarragan@my.gcu.edu>
2024-07-04 01:18:47 -0700

    Removed props from Nav and Footer for SSR

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,16 +14,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Update Company Here
-  const companyName = "ProjectApp";
   return (
     <html lang="en">
       <body className="flex min-h-screen flex-col">
-        <NavBar companyName={companyName} />
+        <NavBar />
         <main className="min-h-screen md:container mx-auto p-5">
           {children}
         </main>
-        <Footer companyName={companyName} />
+        <Footer />
       </body>
       {/* Loands JS for Preline CSS */}
       <PrelineScript />
--- app/ui/footer/page.tsx
+++ app/ui/footer/page.tsx
@@ -1,16 +1,12 @@
 import Link from "next/link";
 
-interface Props {
-  companyName: string;
-}
-
-export default function Footer({ companyName }: Props) {
+export default function Footer() {
   const currentYear = new Date();
   return (
     <footer className="flex justify-center items-center p-4 bg-gray-800 text-white w-full sticky bottom-0">
       <p>
         Copyright &copy; {currentYear.getFullYear()}{" "}
-        <Link href="/">{companyName}</Link>
+        <Link href="/">ProjectApp</Link>
       </p>
     </footer>
   );
