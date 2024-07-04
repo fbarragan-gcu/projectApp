e66e80a6448ea2ef02403c7f9d0a0b154ef63790
Francisco Barragan <fbarragan@my.gcu.edu>
2024-07-04 00:19:33 -0700

    small navbar state changes

--- app/ui/navbar/page.tsx
+++ app/ui/navbar/page.tsx
@@ -1,76 +1,42 @@
-import Link from "next/link";
+"use client";
 import NavLinks from "./Navlinks";
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/client";
 import { getUser } from "@/utils/supabase/queries";
+import { User } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 
 interface Props {
   companyName: string;
 }
 
 // NavBar using NavLinks to controll logged-on or Logged-off state.
-export default async function NavBar({ companyName }: Props) {
+export default function NavBar({ companyName }: Props) {
   // Create Supabase Instance
-  const supabase = createClient();
-  // Check for logged-in user
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const [isLoggedIn, setIsLoggedIn] = useState<User | null | undefined>(null);
+
+  useEffect(() => {
+    async function checkUser() {
+      try {
+        const supabase = createClient();
+        const {
+          data: { user },
+        } = await supabase.auth.getUser();
+        setIsLoggedIn(user);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        // Set State to null if error processing login
+        setIsLoggedIn(null);
+      }
+    }
+
+    checkUser();
+  }, []);
   return (
     <>
       {/* <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4 dark:bg-neutral-800 sticky top-0"> */}
       <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-gray-800 text-sm  text-white py-4 dark:bg-neutral-800 sticky top-0">
-        <NavLinks companyName={companyName} user={user} />
+        <NavLinks companyName={companyName} user={isLoggedIn} />
       </header>
-      {/* DarkMode Toggle */}
-      {/* TODO: FIX */}
-      {/* <button
-        type="button"
-        className="hs-dark-mode-active:hidden block hs-dark-mode group flex items-center text-white hover:text-blue-600 font-medium dark:text-neutral-400 dark:hover:text-neutral-500"
-        data-hs-theme-click-value="dark"
-      >
-        <svg
-          className="flex-shrink-0 size-4"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
-        </svg>
-      </button>
-      <button
-        type="button"
-        className="hs-dark-mode-active:block hidden hs-dark-mode group flex items-center text-white hover:text-blue-600 font-medium dark:text-neutral-400 dark:hover:text-neutral-500"
-        data-hs-theme-click-value="light"
-      >
-        <svg
-          className="flex-shrink-0 size-4"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <circle cx="12" cy="12" r="4"></circle>
-          <path d="M12 2v2"></path>
-          <path d="M12 20v2"></path>
-          <path d="m4.93 4.93 1.41 1.41"></path>
-          <path d="m17.66 17.66 1.41 1.41"></path>
-          <path d="M2 12h2"></path>
-          <path d="M20 12h2"></path>
-          <path d="m6.34 17.66-1.41 1.41"></path>
-          <path d="m19.07 4.93-1.41 1.41"></path>
-        </svg>
-      </button> */}
     </>
   );
 }
