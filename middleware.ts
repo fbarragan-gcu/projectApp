85ccab19ad6095c80f12d50a72e32fb6b485fe6b
Francisco Barragan <fbarragan@my.gcu.edu>
2024-07-07 20:59:38 -0700

    Fixed middleware to allow register, about pages

--- middleware.ts
+++ middleware.ts
@@ -17,6 +17,7 @@ export async function middleware(request: NextRequest) {
   return await updateSession(request)
 }
 
+// TODO: Fix Root /
 export const config = {
   matcher: [
     /*
@@ -26,6 +27,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|about|register|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
